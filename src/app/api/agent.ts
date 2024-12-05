54e6a377c70396127e20bc43876e551497ced352
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2024-12-05 03:55:22 -0300

    feat: connect backend

---
+++ src/app/api/agent.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosResponse } from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.defaults.withCredentials = true;
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const requests = {
+  get: (url: string, params?: URLSearchParams) =>
+    axios.get(url, { params }).then(responseBody),
+  post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
+  put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
+  delete: (url: string) => axios.delete(url).then(responseBody),
+  patch: (url: string, body: {}) => axios.patch(url, body).then(responseBody),
+};
+
+const Auth = {
+  login: (form: any) => requests.post("Auth/login", form),
+  logout: (form: any) => requests.post("Auth/logout", form),
+};
+
+const Users = {
+  list: () => requests.get("user/"),
+  active: () => requests.get("user/ActiveUsers"),
+  updateUser: (form: any) => requests.post("user/UpdateUser", form),
+  changeState: (form: any) => requests.post("user/ChangeState/", form),
+  changePasswordEmployee: (form: any) => requests.post("user/ChangePasswordEmployee", form),
+  changePasswordAdmin: (form: any) => requests.post("user/ChangePasswordAdmin", form),
+};
+
+const agent = { Auth, requests, Users };
+
+export default agent;
--- src/features/home/homepage.tsx
+++ src/features/home/homepage.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import Navbar from "../../app/components/navbar";
 import "../../app/static/styles/index.css";
+import Agent from "../../app/api/agent";
 
 const HomePage = () => {
+
+  const userList = async () => {
+    console.log(process.env.REACT_APP_API_URL);
+  };
+
   return (
     <div>
       <Navbar />
-      <h1>Home Page</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1>Home Page</h1>
+            <button onClick={userList}>List Users</button>
+          </div>
+        </div>
+      </div>
     </div>
   );    
 }
