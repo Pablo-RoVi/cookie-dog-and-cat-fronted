a9e045cc5e9887d20280ec2bcf26573f0c09206e
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2024-12-07 20:39:42 -0300

    feat: create page views

---
+++ src/features/sale/addsalepage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AddSalePage = () => {
+    return (
+        <div>
+            <h1>Add Sale</h1>
+        </div>
+    );
+};
+export default AddSalePage;
---
+++ src/features/sale/editsalepage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const EditSalePage = () => {
+    return (
+        <div>
+            <h1>Editar Venta</h1>
+        </div>
+    );
+};
+export default EditSalePage;
