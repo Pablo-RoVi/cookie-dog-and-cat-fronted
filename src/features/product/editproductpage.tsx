e4372fd02cb9fa297980cf59b6e3df1791cf6f31
2kSebaNG <sebastian.nunez03@alumnos.ucn.cl>
2024-12-11 23:13:52 -0300

    feat: add the data inputs and selects to editProductPage

--- src/features/product/editproductpage.tsx
+++ src/features/product/editproductpage.tsx
@@ -1,9 +1,114 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import TableModule from "../../app/components/tablemodule";
+import Buttons from "../../app/components/buttons";
+import { Product } from "../../app/models/product";
+import Options from '../../app/components/options';
+import { useLocation, useNavigate } from "react-router-dom";
+import cookie from '../../app/static/images/cookie.png';
+
+const defaultProduct: Product = {
+    unique_id: "",
+    product_name: "",
+    price: "",
+    stock: "",
+    categoryName: "",
+    brandName: "",
+    specieName: "",
+};
+
+//TODO: Agregar modal de confirmación, luego redireccionar a la lista de productos
 
 const EditProductPage = () => {
+
+    const [roleFilter, setRoleFilter] = useState<string>("");
+    const [unique_id, setId] = useState<number>(0);
+    const [productName, setName] = useState<string>("");
+    const [price, setPrice] = useState<string>("");
+    const [stock, setStock] = useState<string>("");
+    const [categoryName, setCategoryName] = useState<string>("");
+    const [brandName, setBrandName] = useState<string>("");
+    const [specieName, setEspecieName] = useState<string>("");
+    const [confirmNewPassword, setConfirmNewPassword] = useState<string>("");
+    const location = useLocation();
+    const navigate = useNavigate();
+    const product = location.state;
+
+    
+    useEffect(() => {
+        if (product) {
+            setId(product.unique_id);
+            setName(product.product_name);
+            setPrice(product.price);
+            setStock(product.stock);
+            setCategoryName(product.categoryName);
+            setBrandName(product.brandName);
+            setEspecieName(product.specieName);
+        }
+    }, [product]);
+
+    /*
+    if (!user) {
+        return <div>Usuario no encontrado</div>;
+    }
+    */
+
+    const handleNavigate = () => {
+        navigate('/products');
+    };
+
+
     return (
-        <div>
-            <h1>Edit Product</h1>
+        <div className="max-h-screen bg-white flex-auto flex h-1/2">
+            <div className="container mx-auto mt-6 ml-52">
+                {TableModule.title({title: "Editar producto"})}
+                {TableModule.inputFilter({
+                    label: "Código",
+                    valueFilter: unique_id,
+                })}
+                {TableModule.inputFilter({
+                    label: "Nombre",
+                    valueFilter: productName,
+                    setOnChangeFilter: setName,
+                    placeholder: "Nombre del producto",
+                })}
+                {TableModule.inputFilter({
+                    label: "Precio",
+                    valueFilter: price,
+                    setOnChangeFilter: setPrice,
+                    placeholder: "Precio",
+                })}
+                {TableModule.inputFilter({
+                    label: "Stock",
+                    valueFilter: stock,
+                    setOnChangeFilter: setStock,
+                    placeholder: "Stock",
+                })}
+                {TableModule.selectFilter({
+                    label: "Categoría",
+                    valueFilter: categoryName,
+                    setOnChangeFilter: setCategoryName,
+                    options: Options.categoryOptions,
+                })}
+                {TableModule.selectFilter({
+                    label: "Marca",
+                    valueFilter: brandName,
+                    setOnChangeFilter: setBrandName,
+                    options: Options.brandOptions,
+                })}
+                {TableModule.selectFilter({
+                    label: "Especie",
+                    valueFilter: specieName,
+                    setOnChangeFilter: setEspecieName,
+                    options: Options.specieOptions,
+                })}
+                <div className="flex items-center space-x-4">
+                    <Buttons.TurquoiseButton text="Editar" onClick={handleNavigate} />
+                    <Buttons.FuchsiaButton text="Cancelar" onClick={handleNavigate} />
+                </div>
+            </div>
+            <div className="container mx-auto mt-20">
+                <img src={cookie} alt="cookie" className="h-auto w-auto opacity-10" />
+            </div>
         </div>
     );
 }
