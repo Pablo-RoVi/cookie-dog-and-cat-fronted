fa240a749bfb86d60d961183b6c1f0ae228a87b4
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2024-12-21 19:04:08 -0300

    refactor: update NotFound component to use custom button and improve styling

--- src/features/error/notfound.tsx
+++ src/features/error/notfound.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import Buttons from "../../app/components/buttons";
+import colors from "../../app/static/colors";
 import cookie from "../../app/static/images/cookie.png";
 
 const NotFound = () => {
@@ -13,7 +13,11 @@ const NotFound = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-white">
       <div className="container mx-auto max-w-[20%]">
-        <img src={cookie} alt="cookie" className="h-auto w-auto opacity-10 grayscale" />
+        <img
+          src={cookie}
+          alt="cookie"
+          className="h-auto w-auto opacity-10 grayscale"
+        />
       </div>
       <h1 className="text-8xl font-bold mb-4">¡Oops!</h1>
       <h1 className="text-4xl font-bold mb-4">
@@ -23,7 +27,15 @@ const NotFound = () => {
         Es posible que la página que solicitaste no exista o haya cambiado su
         ubicación.
       </h1>
-      <Buttons.GrayButton text="Volver" onClick={() => handleGoBack()} />
+      <div className="flex justify-end mt-4">
+        <button
+          className="text-white font-bold py-2 px-4 rounded"
+          style={{ backgroundColor: colors.gray }}
+          onClick={handleGoBack}
+        >
+          Volver
+        </button>
+      </div>
     </div>
   );
 };
