31a3e1670bb8ec34b272920168aad5992f7544ff
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2024-12-31 19:24:49 -0300

    test: refactor EditSalePage tests to improve field and button rendering checks and add redirect handling

--- src/test/sale/editsale.test.tsx
+++ src/test/sale/editsale.test.tsx
@@ -1,9 +1,29 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import EditSalePage from "../../features/sale/editsalepage";
 
-jest.mock("../../app/api/agent");
+const mockNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
+
+jest.mock("../../app/api/agent", () => ({
+  Sale: {
+    update: jest.fn().mockResolvedValue({}),
+    getPaymentMethods: jest.fn().mockResolvedValue([
+      { id: 1, name: "Débito" },
+      { id: 2, name: "Transferencia" },
+      { id: 3, name: "Efectivo" },
+      { id: 4, name: "Cheque" },
+      { id: 5, name: "Tarjeta" },
+
+    ]),
+  },
+}));
 
 describe("EditSalePage", () => {
   beforeEach(() => {
@@ -12,13 +32,41 @@ describe("EditSalePage", () => {
 
   const renderComponent = () => {
     render(
-      <BrowserRouter>
+      <Router>
         <EditSalePage />
-      </BrowserRouter>
+      </Router>
     );
   };
 
-  test("renders all fields and buttons", () => {
+  test("renders all fields", () => {
     renderComponent();
+    expect(screen.getByText(/Editar venta/i)).toBeInTheDocument();
+    expect(screen.getByText(/Código/i)).toBeInTheDocument();
+    expect(screen.getByText(/Nombre de usuario/i)).toBeInTheDocument();
+    expect(screen.getByText(/Método de pago/i)).toBeInTheDocument();
+    expect(screen.getAllByText(/Producto/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/Producto/i)[1]).toBeInTheDocument();
+    expect(screen.getAllByText(/Producto/i)[2]).toBeInTheDocument();
+    expect(screen.getByText(/Marca/i)).toBeInTheDocument();
+    expect(screen.getByText(/Categoría/i)).toBeInTheDocument();
+    expect(screen.getByText(/Especie/i)).toBeInTheDocument();
+    expect(screen.getByText(/Precio Unitario/i)).toBeInTheDocument();
+    expect(screen.getByText(/Cantidad/i)).toBeInTheDocument();
+    expect(screen.getByText(/Total producto/i)).toBeInTheDocument();
+  });
+
+  test("renders all buttons", () => {
+    renderComponent();
+    expect(screen.getAllByText(/Editar/i)[1]).toBeInTheDocument();
+    expect(screen.getByText(/Cancelar/i)).toBeInTheDocument();
+  });
+
+  test("redirects on successful edit", async () => {
+    renderComponent();
+
+    const button = screen.getAllByText(/Editar/i)[1];
+
+    fireEvent.click(button);
+
   });
 });
