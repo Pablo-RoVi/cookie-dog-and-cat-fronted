e71e8ee02b15d425d77b62f91035ae8799097159
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2024-12-31 14:19:46 -0300

    test: enhance EditUserPage tests for field and button rendering

--- src/test/user/edituserpage.test.tsx
+++ src/test/user/edituserpage.test.tsx
@@ -1,24 +1,62 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import EditUserPage from "../../features/user/edituserpage";
+import Agent from "../../app/api/agent";
 
-jest.mock("../../app/api/agent");
+const mockNavigate = jest.fn();
 
-describe("EditUserPage", () => {
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
+
+jest.mock("../../app/api/agent", () => ({
+  User: {
+    update: jest.fn(),
+  },
+}));
+
+describe("UserPage", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   const renderComponent = () => {
     render(
-      <BrowserRouter>
+      <Router>
         <EditUserPage />
-      </BrowserRouter>
+      </Router>
     );
   };
 
-  test("renders all fields and buttons", () => {
+  test("renders all fields", () => {
     renderComponent();
+    expect(screen.getByText(/Editar empleado/i)).toBeInTheDocument();
+    expect(screen.getByText(/Código/i)).toBeInTheDocument();
+    expect(screen.getAllByText(/Nombre/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/Nombre/i)[1]).toBeInTheDocument();
+    expect(screen.getAllByText(/Apellido/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/Apellido/i)[1]).toBeInTheDocument();
+    expect(screen.getByText(/RUT/i)).toBeInTheDocument();
+    expect(screen.getByText(/Nombre de usuario/i)).toBeInTheDocument();
+    expect(screen.getByText(/Rol/i)).toBeInTheDocument();
+    expect(screen.getByText(/Editar contraseña/i)).toBeInTheDocument();
+    expect(screen.getByText(/Nueva/i)).toBeInTheDocument();
+    expect(screen.getByText(/Confirmar/i)).toBeInTheDocument();
+  });
+
+  test("renders all buttons", () => {
+    renderComponent();
+    expect(screen.getByPlaceholderText(/Nombre/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/Apellido/i)).toBeInTheDocument();
+    expect(screen.getByText(/SIN ELECCIÓN/i)).toBeInTheDocument();
+    expect(screen.getAllByPlaceholderText(/Alfanumérica y contener al menos 8 caracteres/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByPlaceholderText(/Alfanumérica y contener al menos 8 caracteres/i)[1]).toBeInTheDocument();
+    expect(screen.getAllByText(/Editar/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/Editar/i)[1]).toBeInTheDocument();
+    expect(screen.getAllByText(/Cancelar/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/Cancelar/i)[1]).toBeInTheDocument();
   });
 });
