565480380b5267514ff8d56913dd510c23fd0217
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2024-12-31 15:03:57 -0300

    test: add redirect test on successful edit in EditUserPage

--- src/test/user/edituserpage.test.tsx
+++ src/test/user/edituserpage.test.tsx
@@ -52,11 +52,53 @@ describe("UserPage", () => {
     expect(screen.getByPlaceholderText(/Nombre/i)).toBeInTheDocument();
     expect(screen.getByPlaceholderText(/Apellido/i)).toBeInTheDocument();
     expect(screen.getByText(/SIN ELECCIÓN/i)).toBeInTheDocument();
-    expect(screen.getAllByPlaceholderText(/Alfanumérica y contener al menos 8 caracteres/i)[0]).toBeInTheDocument();
-    expect(screen.getAllByPlaceholderText(/Alfanumérica y contener al menos 8 caracteres/i)[1]).toBeInTheDocument();
+    expect(
+      screen.getAllByPlaceholderText(
+        /Alfanumérica y contener al menos 8 caracteres/i
+      )[0]
+    ).toBeInTheDocument();
+    expect(
+      screen.getAllByPlaceholderText(
+        /Alfanumérica y contener al menos 8 caracteres/i
+      )[1]
+    ).toBeInTheDocument();
     expect(screen.getAllByText(/Editar/i)[0]).toBeInTheDocument();
     expect(screen.getAllByText(/Editar/i)[1]).toBeInTheDocument();
     expect(screen.getAllByText(/Cancelar/i)[0]).toBeInTheDocument();
     expect(screen.getAllByText(/Cancelar/i)[1]).toBeInTheDocument();
+  });
+
+  test("redirects on successful edit", async () => {
+    (Agent.User.update as jest.Mock).mockResolvedValue({
+      data: { id: 1, name: "Camilo", last_name: "Tessini", role: "employee" },
+    });
+
+    renderComponent();
+
+    const nameInput = screen.getByPlaceholderText(/Nombre/i);
+    const lastNameInput = screen.getByPlaceholderText(/Apellido/i);
+    const roleInput = screen.getByText(/SIN ELECCIÓN/i);
+
+    const editButton = screen.getAllByText(/Editar/i)[0];
+
+    fireEvent.change(nameInput, {
+      target: { value: "Camilo" },
+    });
+    fireEvent.change(lastNameInput, {
+      target: { value: "Tessini" },
+    });
+    fireEvent.change(roleInput, {
+      target: { value: "employee" },
+    });
+
+    await fireEvent.click(editButton);
+
+    const editButton2 = screen.getAllByText(/Editar/i)[3];
+
+    await fireEvent.click(editButton2);
+
+    const confirmButton = screen.getByText(/Confirmar/i);
+
+    await fireEvent.click(confirmButton);
   });
 });
