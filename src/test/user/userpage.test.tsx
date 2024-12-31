f02f7d4c82e7dec4851212fbe17c238ef035e82e
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2024-12-30 23:17:18 -0300

    test: mock user data and enhance UserPage tests for field visibility

--- src/test/user/userpage.test.tsx
+++ src/test/user/userpage.test.tsx
@@ -5,11 +5,44 @@ import { BrowserRouter as Router } from "react-router-dom";
 import UserPage from "../../features/user/userpage";
 
 const mockNavigate = jest.fn();
+
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
   useNavigate: () => mockNavigate,
 }));
 
+jest.mock("../../app/api/agent", () => ({
+  User: {
+    list: jest.fn().mockResolvedValue({
+      data: [
+        {
+          id: 1,
+          rut: "17132083-6",
+          name: "Camila",
+          last_name: "Tessini",
+          is_active: true,
+          nick_name: "CTessini",
+          role: {
+            id: 1,
+            role_name: "Admin",
+          },
+        },
+        {
+          id: 2,
+          rut: "20776296-2",
+          name: "Pablo",
+          last_name: "Robledo",
+          is_active: true,
+          nick_name: "PRobledo",
+          role: {
+            id: 2,
+            role_name: "Employee",
+          },
+        },
+      ],
+    }),
+  },
+}));
 describe("UserPage", () => {
   beforeEach(() => {
     jest.clearAllMocks();
@@ -26,6 +59,18 @@ describe("UserPage", () => {
   test("renders all fields and buttons", () => {
     renderComponent();
 
-    expect(screen.getByText(/Empleados/i)).toBeInTheDocument();         
+    expect(screen.getByText(/Empleados/i)).toBeInTheDocument();
+    expect(screen.getAllByText(/Nombre/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/Rol/i)[0]).toBeInTheDocument();
+    expect(screen.getByText(/Estado de la Cuenta/i)).toBeInTheDocument();
+    expect(screen.getByText(/Código/i)).toBeInTheDocument();
+    expect(screen.getByText(/RUT/i)).toBeInTheDocument();
+    expect(screen.getByText(/Apellido/i)).toBeInTheDocument();
+    expect(screen.getByText(/Nombre de Usuario/i)).toBeInTheDocument();
+  });
+
+  test("renders all users", () => {
+    renderComponent();
+    
   });
 });
